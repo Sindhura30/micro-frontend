f9b3ff7ad9ce73936f10f07f9e86207b6b8d79fb
Sindhura Shetty <sindhura.shetty@lowes.com>
2025-08-11 23:47:04 +0530

    fixed css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+import { createGenerateClassName } from "@material-ui/core/styles";
+import { StylesProvider } from "@material-ui/core/styles";
 
 export default () => {
+    const generateClassName = createGenerateClassName({
+      productionPrefix: "co",
+    });
     return (
-        <BrowserRouter>
-            <div>
-            <Header />
-            <MarketingApp />
-            </div>
-        </BrowserRouter>
-    )
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                <Header />
+                <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
+    );
 } 
